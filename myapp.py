0e620ad962d7f6be95879131f1a734423ac40f6d
Jakub Korečko <jakub@korymont.eu>
2021-11-14 21:10:06 +0100

    Create basic layout

--- myapp.py
+++ myapp.py
@@ -1,11 +1,34 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+data = [
+   {
+      'author': 'Author 1',
+      'title': 'Title 1',
+      'content': 'Content 1'
+   },
+   {
+      'author': 'Author 2',
+      'title': 'Title 2',
+      'content': 'Content 2'
+   },
+   {
+      'author': 'Author 3',
+      'title': 'Title 3',
+      'content': 'Content 3'
+   }
+]
+
+
 
 @app.route("/")
 def index():
-   return "Hello World"
+   return render_template('home.html', data=data)
 
+@app.route("/about")
+def about():
+   return "<h1>Hello World about</h1>"
 
 if __name__ == '__main__':
-  app.run(debug=True, port=80)
+  app.run(debug=True, port=1313)
