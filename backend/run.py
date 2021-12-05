4bfcb1c3094018d53f45504d6f45d651bd2bbf3e
Sakonn <jakub@korymont.eu>
2021-12-05 21:53:27 +0100

    Change run command

--- backend/run.py
+++ backend/run.py
@@ -5,4 +5,5 @@ if not sqlalchemy_utils.database_exists('sqlite:///site.db'):
   sqlalchemy_utils.create_database('sqlite:///site.db')
   
 if __name__ == '__main__':
-  app.run(debug=True, port=5000)
+#app.run(debug=True, port=5000)
+  app.run(host="0.0.0.0")
