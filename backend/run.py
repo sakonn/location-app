0b9e3a7e9cef901818b7cd02ef17ee5c0faedd58
Sakonn <jakub@korymont.eu>
2021-12-05 21:41:26 +0100

    Add databse check

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,8 @@
 from locationApp import app
+import sqlalchemy_utils
 
+if not sqlalchemy_utils.database_exists('sqlite:///site.db'):
+  sqlalchemy_utils.create_database('sqlite:///site.db')
+  
 if __name__ == '__main__':
   app.run(debug=True, port=5000)
