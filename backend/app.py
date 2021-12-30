fddca9b5b46d6141ac75696c2bd3225f0ddfb400
Jakub Korečko <jakub@korymont.eu>
2021-12-30 21:51:49 +0100

    Change app deploy config

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,5 @@ from locationApp import db
 app = create_app()
 
 if __name__ == '__main__':
-  app.run(debug=True, port=5000)
+#  app.run(debug=True, port=5000)
+  app.run(host='0.0.0.0', port=5000)
