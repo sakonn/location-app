24e278f27fe766cfe8d06e95e87ce94f522d5fdd
Jakub Korečko <jakub@korymont.eu>
2021-12-30 18:44:26 +0100

    Add requirements.txt file into root of directory

--- backend/locationApp/config.py
+++ backend/locationApp/config.py
@@ -1,9 +1,14 @@
 import os
 from dotenv import load_dotenv
+import urllib.parse 
 
 load_dotenv()
 
 class Config:
+  params = urllib.parse.quote_plus(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_COMMIT_ON_TEARDOWN = True
--- backend/run.py
+++ backend/run.py
@@ -3,8 +3,8 @@ from locationApp import create_app
 import sqlalchemy_utils
 from locationApp import db 
 
-if not sqlalchemy_utils.database_exists(Config.SQLALCHEMY_DATABASE_URI):
-  sqlalchemy_utils.create_database(Config.SQLALCHEMY_DATABASE_URI)
+#if not sqlalchemy_utils.database_exists(Config.SQLALCHEMY_DATABASE_URI):
+#  sqlalchemy_utils.create_database(Config.SQLALCHEMY_DATABASE_URI)
 
 app = create_app()
 
