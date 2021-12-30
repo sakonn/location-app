08ff03a1bcd712c1f9234e71a915eafb26af5fa5
Jakub Korečko <jakub@korymont.eu>
2021-12-30 19:06:34 +0100

    Switch to traditional database conecction

--- backend/locationApp/config.py
+++ backend/locationApp/config.py
@@ -8,7 +8,7 @@ class Config:
   params = urllib.parse.quote_plus(os.environ.get('SQLALCHEMY_DATABASE_URI'))
 
   SECRET_KEY = os.environ.get('SECRET_KEY')
-#  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-  SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#  SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_COMMIT_ON_TEARDOWN = True
