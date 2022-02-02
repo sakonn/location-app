793292395dc9a0638dbbf08751a13ab3afd77c86
Jakub Korečko <jakub@korymont.eu>
2022-02-02 10:06:52 +0100

    Set deploy host to 0.0.0.0

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,5 @@ from locationApp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-  app.run(debug=True, port=5000)
-#  app.run(host='0.0.0.0', port=5000)
+# app.run(debug=True, port=5000)
+  app.run(host='0.0.0.0', port=5000)
